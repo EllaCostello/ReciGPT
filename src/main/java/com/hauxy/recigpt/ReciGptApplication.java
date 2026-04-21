336ec412331c01b87531c6606222438dc13c8fea
Ella Costello <ella.costello@yahoo.com>
2026-04-21 12:14:15 +0200

    Starter app

---
+++ src/main/java/com/hauxy/recigpt/ReciGptApplication.java
@@ -0,0 +1,13 @@
+package com.hauxy.recigpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciGptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciGptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hauxy/recigpt/ReciGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hauxy.recigpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
